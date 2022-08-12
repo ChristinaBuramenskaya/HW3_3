c6fa3799c5a7bcb61a1cdd86ff3bd18ee0f70e62
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-13 02:18:43 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите x первой точки");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y первой точки");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z первой точки");
+int zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x второй точки");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y второй точки");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z второй точки");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double S=(Math.Sqrt(Math.Pow((xB-xA),2)+Math.Pow((yB-yA),2)+Math.Pow((zB-zA),2)));
+Console.WriteLine(S);
