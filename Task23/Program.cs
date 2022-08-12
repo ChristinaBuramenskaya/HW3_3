d070e5361133e191542c2a2cc70ed4d7f6076559
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-13 02:25:43 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+double result = 0;
+
+while (count <= x)
+{
+    result = Math.Pow(count, 3);
+    Console.WriteLine(result);
+    count++;
+}
