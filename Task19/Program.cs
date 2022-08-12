8e185085e53ee23e3c46f64c4c1205351c295b90
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-12 23:26:09 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+if (number[0]==number[4] && number[1]==number[3])
+{
+    Console.WriteLine("Да, это палиндром");
+}
+else
+{
+    Console.WriteLine("Нет, это не палиндром");
+}
+
+
